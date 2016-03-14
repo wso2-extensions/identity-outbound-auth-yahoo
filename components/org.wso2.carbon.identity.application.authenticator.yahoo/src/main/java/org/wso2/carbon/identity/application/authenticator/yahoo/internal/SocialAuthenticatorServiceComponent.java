e2d43c3e1fa92530132d5b04f2c92c6c14b58a80
godwin <godwinamila@gmail.com>
2016-03-14 14:27:35 +0530

    Remove unnecessary authenticators

--- components/org.wso2.carbon.identity.application.authenticator.yahoo/src/main/java/org/wso2/carbon/identity/application/authenticator/yahoo/internal/SocialAuthenticatorServiceComponent.java
+++ components/org.wso2.carbon.identity.application.authenticator.yahoo/src/main/java/org/wso2/carbon/identity/application/authenticator/yahoo/internal/SocialAuthenticatorServiceComponent.java
@@ -22,9 +22,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.identity.application.authentication.framework.ApplicationAuthenticator;
-import org.wso2.carbon.identity.application.authenticator.yahoo.facebook.FacebookAuthenticator;
-import org.wso2.carbon.identity.application.authenticator.yahoo.google.GoogleOAuth2Authenticator;
-import org.wso2.carbon.identity.application.authenticator.yahoo.live.WindowsLiveOAuth2Authenticator;
 import org.wso2.carbon.identity.application.authenticator.yahoo.YahooOAuth2Authenticator;
 
 /**
@@ -38,34 +35,22 @@ public class SocialAuthenticatorServiceComponent {
     protected void activate(ComponentContext ctxt) {
         try {
 
-            FacebookAuthenticator facebookAuthenticator = new FacebookAuthenticator();
-            ctxt.getBundleContext().registerService(ApplicationAuthenticator.class.getName(),
-                    facebookAuthenticator, null);
-
-            WindowsLiveOAuth2Authenticator windowsLiveOAuth2Authenticator = new WindowsLiveOAuth2Authenticator();
-            ctxt.getBundleContext().registerService(ApplicationAuthenticator.class.getName(),
-                    windowsLiveOAuth2Authenticator, null);
-
-            GoogleOAuth2Authenticator googleAuthenticator = new GoogleOAuth2Authenticator();
-            ctxt.getBundleContext().registerService(ApplicationAuthenticator.class.getName(),
-                    googleAuthenticator, null);
-
             YahooOAuth2Authenticator yahooOAuth2Authenticator = new YahooOAuth2Authenticator();
             ctxt.getBundleContext().registerService(ApplicationAuthenticator.class.getName(),
                     yahooOAuth2Authenticator, null);
 
             if (log.isDebugEnabled()) {
-                log.debug("Social Authenticator bundle is activated.");
+                log.debug("Yahoo Social Authenticator bundle is activated.");
             }
 
         } catch (Throwable e) {
-            log.fatal("Error while activating Social authenticator bundle.", e);
+            log.fatal("Error while activating Yahoo Social authenticator bundle.", e);
         }
     }
 
     protected void deactivate(ComponentContext ctxt) {
         if (log.isDebugEnabled()) {
-            log.debug("Social Authenticator bundle is deactivated.");
+            log.debug("Yahoo Social Authenticator bundle is deactivated.");
         }
     }
 }
